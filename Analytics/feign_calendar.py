027b8febbbeff1b73f71165da9d74180ef01874d
Tyson Morris <temorris87@yahoo.com>
2019-08-30 12:51:18 -0400

    Creation of calendar events completed.

--- Analytics/feign_calendar.py
+++ Analytics/feign_calendar.py
@@ -7,13 +7,13 @@ from google_auth_oauthlib.flow import InstalledAppFlow
 from google.auth.transport.requests import Request
 
 # If modifying these scopes, delete the file token.pickle.
-SCOPES = ['https://www.googleapis.com/auth/calendar.readonly']
+SCOPES = ['https://www.googleapis.com/auth/calendar']
 
 
-def main():
+def login():
     """Shows basic usage of the Google Calendar API.
-    Prints the start and name of the next 10 events on the user's calendar.
-    """
+        Prints the start and name of the next 10 events on the user's calendar.
+        """
     creds = None
     # The file token.pickle stores the user's access and refresh tokens, and is
     # created automatically when the authorization flow completes for the first
@@ -33,21 +33,74 @@ def main():
         with open('token.pickle', 'wb') as token:
             pickle.dump(creds, token)
 
-    service = build('calendar', 'v3', credentials=creds)
+    return build('calendar', 'v3', credentials=creds)
 
+
+def get_events(num_events):
+    service = login()
     # Call the Calendar API
     now = datetime.datetime.utcnow().isoformat() + 'Z' # 'Z' indicates UTC time
-    print('Getting the upcoming 10 events')
+    print(f'Getting the upcoming {num_events} events')
     events_result = service.events().list(calendarId='primary', timeMin=now,
-                                        maxResults=10, singleEvents=True,
+                                        maxResults=num_events, singleEvents=True,
                                         orderBy='startTime').execute()
     events = events_result.get('items', [])
 
     if not events:
         print('No upcoming events found.')
     for event in events:
-        start = event['start'].get('dateTime', event['start'].get('date'))
-        print(start, event['summary'])
+        print(event)
+        # start = event['start'].get('dateTime', event['start'].get('date'))
+        # print(start, event['summary'])
+
+
+def build_date_time(date, time, gmt):
+    return f'{date}T{time}{gmt}'
+
+
+def get_time_zone(gmt):
+    time_zones = {
+        "-04:00": 'America/New_York'
+    }
+    return time_zones.get(gmt)
+
+
+def set_events(date, start_time, end_time, gmt, description, location, title):
+    service = login()
+    event = {
+        'summary': f'{title}',
+        'location': f'{location}',
+        'description': f'{description}',
+        'start': {
+            'dateTime': build_date_time(date, start_time, gmt),
+            'timeZone': get_time_zone(gmt),
+        },
+        'end': {
+            'dateTime': build_date_time(date, end_time, gmt),
+            'timeZone': get_time_zone(gmt),
+        },
+        'recurrence': [
+            # 'RRULE:FREQ=DAILY;COUNT=2'
+        ],
+        'attendees': [
+            # {'email': 'lpage@example.com'},
+            # {'email': 'sbrin@example.com'},
+        ],
+        'reminders': {
+            # 'useDefault': False,
+            # 'overrides': [
+            #     {'method': 'email', 'minutes': 24 * 60},
+            #     {'method': 'popup', 'minutes': 10},
+            # ],
+        },
+    }
+
+    event = service.events().insert(calendarId='primary', body=event).execute()
+    print('Event created: %s' % (event.get('htmlLink')))
+
+
+def main():
+    set_events("2019-09-01", "13:00:00", "14:00:00", "-04:00", "This is a test calendar event.", "New York City", "Sample Title")
 
 
 if __name__ == '__main__':
